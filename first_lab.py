f63bf9cfe1a722bfea4ce68cab7bc826ddbbdce5
hadarco24-meet <hadari0808@gmail.com>
2024-07-14 17:22:08 +0300

    worked on lab

---
+++ first_lab.py
@@ -0,0 +1,15 @@
+import random
+temperatures = [random.randint(26,41)]
+good_days_count = []
+days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+for i in range(len(days_of_the_week)):
+	if ((temperatures(i) % 2) == 0):
+		good_days_count.append(days_of_the_week(i))
+
+print(good_days_count)
+print(days_of_the_week)
+
+
+
+
+
